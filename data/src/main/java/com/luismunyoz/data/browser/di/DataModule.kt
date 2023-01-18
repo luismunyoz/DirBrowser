a781b56be6f4ee1a57ef5dd0166cf5bd9b77bac2
Luis Muñoz <luis.n90@gmail.com>
2023-01-18 17:36:23 +0100

    Make injection to work

--- data/src/main/java/com/luismunyoz/data/browser/di/DataModule.kt
+++ data/src/main/java/com/luismunyoz/data/browser/di/DataModule.kt
@@ -1,33 +1,11 @@
 package com.luismunyoz.data.browser.di
 
-import com.luismunyoz.data.browser.BrowserApi
 import com.luismunyoz.data.browser.BrowserRepository
 import com.luismunyoz.domain.browser.BrowserRepositoryContract
-import com.luismunyoz.network.di.BaseURLQualifier
 import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.OkHttpClient
-import retrofit2.Retrofit
-
-@Module
-@InstallIn(SingletonComponent::class)
-object ApiModule {
-
-    @Provides
-    fun provideBrowserApi(
-        @BaseURLQualifier baseUrl: String,
-        okHttpClient: OkHttpClient
-    ): BrowserApi {
-        return Retrofit.Builder()
-            .baseUrl(baseUrl)
-            .client(okHttpClient)
-            .build()
-            .create(BrowserApi::class.java)
-    }
-}
 
 @Module
 @InstallIn(SingletonComponent::class)
